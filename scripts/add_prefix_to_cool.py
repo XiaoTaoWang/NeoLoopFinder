e66ea25a29f4ec6eed65d090c3c7d017014f3882
Xiaotao Wang <wangxiaotao686@gmail.com>
2021-06-08 15:46:41 -0500

    deal with hic2cool

---
+++ scripts/add_prefix_to_cool.py
@@ -0,0 +1,8 @@
+import cooler, sys
+
+in_cool = sys.argv[1]
+
+clr = cooler.Cooler(in_cool)
+chromlist = clr.chromnames
+chrom_map = {c:'chr'+c for c in chromlist}
+cooler.rename_chroms(clr, chrom_map)
